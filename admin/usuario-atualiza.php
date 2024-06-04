95f2629a8439e3eb0be98c3f57178eb7a7ce8562
Mmarli18 <mahmorais0@gmail.com>
2024-06-04 14:57:53 -0300

    mostrando apenas array

--- admin/usuario-atualiza.php
+++ admin/usuario-atualiza.php
@@ -1,6 +1,13 @@
 <?php 
 require_once "../inc/cabecalho-admin.php";
+require_once "../inc/funcoes-usuarios.php";
+
+// Pegando o valor do parâmetro id vindo da url
+$id = $_GET['id'];
+
+$dadosUsuario = lerUmUsuario($conexao, $id);
 ?>
+<pre><?=var_dump($dadosUsuario)?></pre>
 
 
 <div class="row">
